33f28624b6aa4cd3a39380e0fdd7976d1f430eed
侯光强 <houguangqiang@shsnc.com>
2018-01-02 21:07:20 +0800

    初始化项目

--- src/main/java/com/example/demo/MyAppApplication.java
+++ src/main/java/com/example/demo/MyAppApplication.java
@@ -3,8 +3,16 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.IOException;
+
+@Controller
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MyAppApplication {
 
@@ -12,6 +20,15 @@ public class MyAppApplication {
 	public String hello(){
 		return "World!!!";
 	}
+
+    @GetMapping("download")
+    public ResponseEntity<byte[]> download(String fileName) throws IOException {
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+	    headers.setContentDispositionFormData("attachment", "abc.txt");
+	    return new ResponseEntity<byte[]>("Hello".getBytes(), headers, HttpStatus.CREATED);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyAppApplication.class, args);
